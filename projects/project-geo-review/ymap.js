565ad68bf953e60109c3a81733f107c6988a8b66
EmpyreanAquilon <empyreanaquilon@yandex.ru>
2021-12-07 12:30:53 +0500

    trying_2

--- projects/project-geo-review/ymap.js
+++ projects/project-geo-review/ymap.js
@@ -1,3 +1,4 @@
+// eslint no-use-before-define: 0
 ymaps.ready(init);
 
 function init() {
@@ -7,60 +8,52 @@ function init() {
         // controls: ['searchControl']
     });
 
-    // создал плейсмарк со своим контентом
-    // const placemark_1 = new ymaps.Placemark([56.820842, 60.625474], {
-    //     hintContent: 'Это Хинт',
-    //     balloonContent: ['<form class="feedback">', '<h3>Отзыв:</h3>', '<input type="text" class="feedback__input" placeholder="Укажите ваше имя">', '<input type="text" class="feedback__input" placeholder="Укажите место">', '<input type="text" class="feedback__input" placeholder="Оставить отзыв">', '<button class="feedback__btn">Добавить</button>'].join(''),
-    // });
-    // map.geoObjects.add(placemark_1);
+    // массив с данными 
+    let dataPlace = [];
+    class Data {
+        constructor(coords, name, place, review) {
+            this.coords = coords;
+            this.name = name;
+            this.place = place;
+            this.review = review;
+        }
+    };
+
+    const formMarkup = ['<form class="fb" id="fb">', '<h3>Отзыв:</h3>', '<input type="text" class="fb__input fb__input_1" placeholder="Укажите ваше имя">', '<input type="text" class="fb__input fb__input_2" placeholder="Укажите место">', '<input type="text" class="fb__input fb__input_3" placeholder="Оставить отзыв">', '<button class="fb__btn add" id="addBtn">Добавить</button>'];
 
     map.events.add('click', (e) => {
         const coords = e.get('coords');
-        // открываю балун со своими данными
         if (!map.balloon.isOpen()) {
             map.balloon.open(coords, {
-                content: ['<form class="feedback">', '<h3>Отзыв:</h3>', '<input type="text" class="feedback__input_1" placeholder="Укажите ваше имя">', '<input type="text" class="feedback__input_2" placeholder="Укажите место">', '<input type="text" class="feedback__input_3" placeholder="Оставить отзыв">', '<button class="feedback__btn add" id="addBtn">Добавить</button>'].join('')
+                content: formMarkup.join('')
+            }).then(() => {
+                const addBtn = document.getElementById('addBtn');
+                addBtn.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    const feedback = document.getElementById('fb');
+                    let input_1 = feedback.elements[0].value;
+                    let input_2 = feedback.elements[1].value;
+                    let input_3 = feedback.elements[2].value;
+                    if (input_1 && input_2 && input_3) {
+                        dataPlace.push(new Data(coords, input_1, input_2, input_3));
+                        console.log(dataPlace);
+                        const mark = new ymaps.Placemark(coords, {
+                            balloonContent: formMarkup.join('')
+                        });
+                        map.geoObjects.add(mark)
+                        map.balloon.close();
+                    } else {
+                        console.log('заполните поля')
+                    }
+                });
             });
-
-        const addBtn = document.getElementById('addBtn');
-        // addBtn.addEventListener('click', (e) => {
-        //     e.preventDefault();
-        // });
-        console.log(document.getElementById('addBtn'))
-            
         } else {
             map.balloon.close()
         }
-        
-
-        // получаем значения из полей ввода
-
-
-        // и пытаюсь навесить обработчик на кнопку, но не успешно
-        // const feedbackBtn = document.querySelectorAll('.feedback__btn');
-        // if (feedbackBtn) {
-        //     feedbackBtn.forEach((btn) => {
-        //         btn.addEventListener('click', (e) => {
-        //             e.preventDefault();
-        //             console.log('есть кнопка')
-        //         })
-        //     }
-        // )}
-
-        // создаю свою форму, что не верно
-        // var form = document.createElement('form');
-        // document.body.prepend(form);
-        // form.outerHTML = '<form class="feedback"><h3>Отзыв:</h3><input type="text" class="feedback__input" placeholder="Укажите ваше имя"><input type="text" class="feedback__input" placeholder="Укажите место"><input type="text" class="feedback__input" placeholder="Оставить отзыв"><button class="feedback__btn">Добавить</button></form>';
-
-
-
-        // const mark = new ymaps.Placemark(coords, {
-        //     balloonContent: ['<form class="feedback">', '<h3>Отзыв:</h3>', '<input type="text" class="feedback__input" placeholder="Укажите ваше имя">', '<input type="text" class="feedback__input" placeholder="Укажите место">', '<input type="text" class="feedback__input" placeholder="Оставить отзыв">', '<button class="feedback__btn add">Добавить</button>'].join('')
-        // });
-        // var addBtn = document
-        // map.geoObjects.add(mark);
-        // mark.balloon.open();
     });
+    // map.geoObjects.events.add('click', (e) => {
+    //     console.log('есть')
+    // }
 
     // placemark.events.add('click', (e) => {
 
